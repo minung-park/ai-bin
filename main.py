9e3dc8c876f62f701da2f2ed370642eee4d9b671
minung-park <my.park@lawcompany.co.kr>
2022-05-17 18:22:18 +0900

    description update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # @created: 2022-01-27
 # @last_updated: 2022-01-27
 # 아무런 역할을 하지않는 깡통 api 입니다. 중간 브릿지 역할만을 담당합니다.
+# bool value 가 파라미터에 포함되어야 할 경우 "true" 혹은 "false" 처럼 string 값으로 보내면 됩니다.
 
 
 from flask import Flask, request
